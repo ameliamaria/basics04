dbbfa524629bd233075e08af3e5d41eba8e5e63a
ameliamaria <ameliamaria.maciorowska@gmail.com>
2020-05-07 14:52:49 +0200

    add homework draft

---
+++ basics04.py
@@ -0,0 +1,35 @@
+list_a = [1, 2, 3, 4, 5, 6, 7]
+
+print("#1")
+print(list_a[0:7:2])
+
+print("#2")
+print(list_a[1:7:2])
+
+list_a = [1, 2, 3, 4, 5, 6, 7]
+print("#3")
+list_a.reverse()
+print(list_a)
+
+list_a = [1, 2, 3, 4, 5, 6, 7]
+print("#4")
+list_a.reverse()
+print(list_a[1:7:2])
+
+list_a = [1, 2, 3, 4, 5, 6, 7]
+print("#5")
+print(list_a[0:7:6])
+
+list_a = [1, 2, 3, 4, 5, 6, 7]
+print("#6")
+list_a.reverse()
+print(list_a[0:7:3])
+
+list_a = [1, 2, 3, 4, 5, 6, 7]
+print("#7")
+list_a.clear()
+print(list_a)
+
+list_a = [1, 2, 3, 4, 5, 6, 7]
+print("#8")
+print(list_a[4:7])
